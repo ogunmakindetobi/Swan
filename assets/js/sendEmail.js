2000164fee6a54a95bd81125f9ea82bfd67ccb13
ogunmakindetobi <ogunmakindetobi@outlook.com>
2020-04-24 03:58:21 +0000

    Implement emailjs integration on the index  page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm){
+emailjs.send("gmail", "swan", {
+    "from_name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "from_mobile": contactForm.mobile.value,
+    "from_Date": contactForm.date.value,
+
+})
+
+.then(
+    function(response){
+        console.log("SUCCESS", response);
+    },
+   function(error) {
+       console.log("FALIED", error);
+   },
+   
+);
+return false;
+}
